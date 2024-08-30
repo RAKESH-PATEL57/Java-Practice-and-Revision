7d41eff1ceba05a97467ed2b6038945c02d82a11
Rakesh Patel <rakesh246808@gmail.com>
2024-08-30 22:58:05 +0530

    updated

---
+++ leetcode_Practice/CountingBits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] countBits(int n) {
+        
+        int res[] = new int[n+1];
+        
+        for(int i = 0 ;i<=n;i++)
+        {
+            res[i] = Integer.bitCount(i);
+        } 
+            
+        return res;
+    }
+
+ 
+}
