0dfd7737faa7fc9acb9b4b9cd6de628c53084e5d
Rakesh Patel <rakesh246808@gmail.com>
2024-12-05 12:36:11 +0530

    updated

---
+++ GFG/StringsRotationsofEachOther.java
@@ -0,0 +1,24 @@
+package GFG;
+
+import java.util.Arrays;
+
+public class StringsRotationsofEachOther {
+    public static boolean areRotations(String s1, String s2) 
+    {
+        StringBuilder temp = new StringBuilder(s2);
+        temp.append('$').append(s1).append(s1);
+        int n = temp.length(),i=0,j=1,m=s2.length();
+        int lps[] = new int[n];
+        Arrays.fill(lps,0);
+        String str = temp.toString();
+        while(j<n){
+            if(str.charAt(i)==str.charAt(j)){
+                lps[j++]=++i;
+                if(i==m)return true;
+            }
+            else if(i!=0)i=lps[i-1];
+            else j++;
+        }
+        return false;
+    }
+}
