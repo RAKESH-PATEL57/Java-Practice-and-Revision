91b8b549401688fc5bdc5df4373dd2bf7377a0a0
Rakesh Patel <rakesh246808@gmail.com>
2024-12-16 14:12:27 +0530

    updated

---
+++ GFG/SearchinRotatedSortedArray.java
@@ -0,0 +1,28 @@
+package GFG;
+
+public class SearchinRotatedSortedArray {
+    int search(int[] arr, int key) 
+    {
+        int ln = arr.length;
+        int left=0;
+        int right=ln-1;
+        int res = -1;
+
+        while(left<=right)
+        {
+            if(arr[left] == key)
+            {
+                return left;
+            }
+            else if(arr[right] == key)
+            {
+                return right;
+            }
+            left++;
+            right--;
+        }   
+        5a221bf
+
+        return res; 
+    }
+}
