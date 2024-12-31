1bf87ecbca5336d43760bc9b6483ef6fe53aa71f
Rakesh Patel <rakesh246808@gmail.com>
2024-12-31 07:27:08 +0530

    updated

---
+++ GFG/UnionofArrayswithDuplicates.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class UnionofArrayswithDuplicates {
+      public static int findUnion(int a[], int b[]) {
+      
+       HashMap<Integer,Integer> temp = new HashMap<>();
+       int res=0;
+       
+       for(int i=0;i<a.length;i++)
+       {
+           if( temp.get(a[i]) == null)
+           {
+               res++;
+           }
+           temp.put(a[i],1);
+       }
+       
+       for(int i=0;i<b.length;i++)
+       {
+           if( temp.get(b[i]) == null)
+           {
+               res++;
+           }
+           temp.put(b[i],1);
+       }
+       
+       return res;
+    }
+}
