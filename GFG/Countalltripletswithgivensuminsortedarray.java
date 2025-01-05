bebb4dafea180168775c30f1b11b0b9e3aee8566
Rakesh Patel <rakesh246808@gmail.com>
2025-01-05 08:25:12 +0530

    Countalltripletswithgivensuminsortedarray problem solved

---
+++ GFG/Countalltripletswithgivensuminsortedarray.java
@@ -0,0 +1,27 @@
+package GFG;
+
+public class Countalltripletswithgivensuminsortedarray {
+    public int countTriplets(int[] arr, int target) {
+        int count=0;
+        int n=arr.length;
+        for(int i=0;i<=n-3;i++){
+            int j=i+1,k=n-1;
+            while(j<k){
+                int sum=arr[i]+arr[j]+arr[k];
+                if(sum>target) k--;
+                else if(sum<target)j++;
+                else if(sum==target){
+                    count++;
+                    int temp=j+1;
+                    while(temp<k&& arr[temp]==arr[temp-1])
+                    {
+                        count++;
+                        temp++;
+                    }
+                    k--;
+                }
+            }
+        }
+        return count;
+    }
+}
