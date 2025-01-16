d187515a7214e160557133f4433375682f747a91
Rakesh Patel <rakesh246808@gmail.com>
2025-01-16 19:22:32 +0530

    Longest Subarray with Sum K problem is solved

---
+++ GFG/LongestSubarraywithSumK.java
@@ -0,0 +1,28 @@
+package GFG;
+
+import java.util.HashMap;
+
+public class LongestSubarraywithSumK {
+     public int longestSubarray(int[] arr, int k) 
+    {
+        HashMap<Integer,Integer> prefixSum = new HashMap<>();
+        int n=arr.length,sum=0,ans=0;
+        for(int i=0;i<n;i++)
+        {
+            sum += arr[i];
+            if(sum == k)
+            {
+                ans = i+1;
+            }
+            else if(prefixSum.get(sum-k) != null)
+            {
+                ans = Math.max(ans,i-prefixSum.get(sum-k));
+            }
+            if(prefixSum.get(sum) == null)
+            {
+                prefixSum.put(sum,i);
+            }
+        }
+        return ans;
+    }
+}
