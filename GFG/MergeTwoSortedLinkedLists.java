2e8acfb0baa652b043a4706b61bc293ae10ac896
Rakesh Patel <rakesh246808@gmail.com>
2025-01-21 09:14:38 +0530

    Merge Two Sorted Linked Lists problem is solved

---
+++ GFG/MergeTwoSortedLinkedLists.java
@@ -0,0 +1,27 @@
+package GFG;
+
+import org.w3c.dom.Node;
+
+public class MergeTwoSortedLinkedLists {
+     Node sortedMerge(Node head1, Node head2) {
+        Node p1=head1, p2=head2, temp=new Node(-1),head=temp;
+        
+        while(p1!=null && p2!=null)
+        {
+            if(p1.data<=p2.data)
+            {
+                temp.next=p1;
+                p1=p1.next;
+            }
+            else
+            {
+                temp.next=p2;
+                p2=p2.next;
+            }
+            temp=temp.next;
+        }
+        
+        temp.next = p1!=null?p1:p2;
+        return head.next;
+    }
+}
