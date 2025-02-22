010756640576fc89ed530ce1ffac10ec227bceb0
Rakesh Patel <rakesh246808@gmail.com>
2025-02-22 12:20:37 +0530

    Longest Valid Parentheses problem is solved

---
+++ GFG/LongestValidParentheses.java
@@ -0,0 +1,26 @@
+package GFG;
+
+import java.util.Stack;
+
+public class LongestValidParentheses {
+     static int maxLength(String s) 
+    {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLen = Math.max(maxLen, i - stack.peek());
+                }
+            }
+        }
+
+        return maxLen;
+    }
+}
