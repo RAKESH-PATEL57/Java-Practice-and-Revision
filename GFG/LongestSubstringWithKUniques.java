c468b116190d3a2440e5b53fc9fbd6ccc7dfa37a
Rakesh Patel <rakesh246808@gmail.com>
2025-03-08 22:32:15 +0530

    Longest Substring with K Uniques problem is solved

---
+++ GFG/LongestSubstringWithKUniques.java
@@ -0,0 +1,49 @@
+package GFG;
+
+public class LongestSubstringWithKUniques {
+    public int longestkSubstr(String s, int k) {
+        int n = s.length();
+        int windowStart = 0;
+        int windowEnd = 0;
+        int len = 0;
+        int maxLen = Integer.MIN_VALUE;
+        int freqHash[] = new int[26];
+        int uniqueCharCount = 0;
+        while(windowEnd<n)
+        {
+            int index = s.charAt(windowEnd) - 97;
+            if(freqHash[index] == 0)
+            {
+                uniqueCharCount++;
+                freqHash[index]++;
+            }
+            else
+            {
+                freqHash[index]++;
+            }
+            if(uniqueCharCount == k)
+            {
+                len = windowEnd-windowStart+1;
+                maxLen = Math.max(maxLen,len);
+            }
+            else if(uniqueCharCount>k)
+            {
+                while(windowStart<windowEnd && uniqueCharCount>k)
+                {
+                    index = s.charAt(windowStart)-97;
+                    windowStart++;
+                    freqHash[index]--;
+                    if(freqHash[index] == 0)
+                    {
+                        uniqueCharCount--;
+                    }
+                }
+            }
+            windowEnd++;
+        }
+        
+        
+        return (maxLen == Integer.MIN_VALUE)?-1:maxLen;
+        
+    }
+}
