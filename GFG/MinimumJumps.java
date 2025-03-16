2a866ea0ce13fbde8e7807110da56330e7dc7357
Rakesh Patel <rakesh246808@gmail.com>
2025-03-16 20:43:08 +0530

    Minimum Jumps problem is solved by different approach

--- GFG/MinimumJumps.java
+++ GFG/MinimumJumps.java
@@ -1,7 +1,11 @@
 package GFG;
 
+import java.util.Arrays;
+
 public class MinimumJumps 
 {
+
+    //approach - 1
     static int minJumps(int[] arr) 
     {
         if(arr[0]==0)return -1;
@@ -18,5 +22,26 @@ public class MinimumJumps
             }
         }
         return ans;
-    }    
+    } 
+    // approach - 2
+     static int minJumps(int[] arr) {
+        int n = arr.length;
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[n - 1] = 0;
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j <= i + arr[i] && j < n; j++) {
+                if (dp[j] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], 1 + dp[j]);
+                }
+            }
+        }
+
+        if (dp[0] == Integer.MAX_VALUE)
+            return -1;
+
+        return dp[0];
+    }
 }
