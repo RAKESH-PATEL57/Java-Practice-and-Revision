f681586bc32681d03aab9fe0b9de16dc6130cdba
Rakesh Patel <rakesh246808@gmail.com>
2025-05-23 19:19:19 +0530

    Dice throw problem is solved

---
+++ GFG/DiceThrow.java
@@ -0,0 +1,30 @@
+package GFG;
+
+public class DiceThrow {
+    static int noOfWays(int m, int n, int x) 
+    {
+
+        int[] dp = new int[x + 1];
+
+        dp[0] = 1;
+        
+        for (int i = 1; i <= n; i++) 
+        {
+
+            for (int j = x; j >= 1; j--) 
+            {
+
+                dp[j] = 0;
+
+                for (int k = 1; k <= m && j - k >= 0; k++) 
+                {
+                    dp[j] += dp[j - k];
+                }
+            }
+
+            dp[0] = 0;
+        }
+        
+        return dp[x];
+    }
+}
