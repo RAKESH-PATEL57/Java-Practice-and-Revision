61508342166f78cdcaca278faec4a9c3f985c080
Rakesh Patel <rakesh246808@gmail.com>
2025-06-08 19:51:57 +0530

    Row with max 1s problem is solved

---
+++ GFG/RowWithMax1s.java
@@ -0,0 +1,40 @@
+package GFG;
+
+public class RowWithMax1s {
+    public int rowWithMax1s(int arr[][]) {
+        // code here
+        int rlen = arr.length;
+        int clen = arr[0].length;
+        int res = -1;
+        
+        int tempSum = 0;
+        int sum=0;
+        
+        for(int i=0;i<rlen;i++)
+        {
+            for(int j=0;j<clen;j++)
+            {
+                if(arr[i][j] == 1)
+                {
+                  tempSum += 1;
+                }
+            }
+            
+            if(tempSum > sum)
+            {
+                res = i;
+                sum = tempSum;
+            }
+            
+            
+            // System.out.println(sum + " " + res);
+           
+            
+            tempSum = 0;
+        }
+        
+        
+        return res;
+        
+    }
+}
