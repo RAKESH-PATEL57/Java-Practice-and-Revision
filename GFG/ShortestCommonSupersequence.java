baf81e2004cf1d5247ce5341514ed132f2c53960
Rakesh Patel <rakesh246808@gmail.com>
2025-06-17 22:54:04 +0530

    Shortest Common Supersequence

---
+++ GFG/ShortestCommonSupersequence.java
@@ -0,0 +1,44 @@
+package GFG;
+
+public class ShortestCommonSupersequence {
+     public static int shortestCommonSupersequence(String s1, String s2) {
+       
+        int m = s1.length();
+        int n = s2.length();
+        
+        
+        int[][] dp = new int[m + 1][n + 1];
+        
+
+        for (int j = 0; j <= n; j++) 
+        {
+            dp[0][j] = j;
+        }
+        
+       
+        for (int i = 0; i <= m; i++) 
+        {
+            dp[i][0] = i;
+        }
+        
+    
+        for (int i = 1; i <= m; i++) 
+        {
+            for (int j = 1; j <= n; j++) 
+            {
+            
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) 
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+              
+                else 
+                {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
