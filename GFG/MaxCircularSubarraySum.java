432252f323ac0af0c4d26d3816fbdbc3546e0a6e
Rakesh Patel <rakesh246808@gmail.com>
2025-07-25 23:57:59 +0530

    Max Circular Subarray Sum problem is solved using different approach

--- GFG/MaxCircularSubarraySum.java
+++ GFG/MaxCircularSubarraySum.java
@@ -27,5 +27,46 @@ public class MaxCircularSubarraySum
         
         return Math.max(maxSum, res-minSum);
         // Your code here
-    }    
+    }  
+    
+    //method-2
+    public int maxCircularSum(int[] arr) {
+    int n = arr.length;
+    
+    // Case 1: Find maximum subarray sum using Kadane's algorithm
+    int maxKadane = kadane(arr);
+    
+    // Case 2: Find maximum circular subarray sum
+    int totalSum = 0;
+    for (int i = 0; i < n; i++) {
+        totalSum += arr[i];
+        // Invert the signs of array elements
+        arr[i] = -arr[i];
+    }
+    
+    // Find min subarray sum in inverted array (which is max in original)
+    int maxCircular = totalSum + kadane(arr);
+    
+    // If all elements are negative, maxCircular will be 0
+    // In that case, return maxKadane
+    if (maxCircular == 0) {
+        return maxKadane;
+    }
+    
+    // Return the maximum of the two cases
+    return Math.max(maxKadane, maxCircular);
+}
+
+// Kadane's algorithm to find maximum subarray sum
+private int kadane(int[] arr) {
+    int maxSoFar = arr[0];
+    int maxEndingHere = arr[0];
+    
+    for (int i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+    
+    return maxSoFar;
+}
 }
